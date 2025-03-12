530a22ae30fea0b7943ca8bdb6d88b43055ada11
fernando-apollo <fernando@apollographql.com>
2025-03-12 16:29:29 +0000

    added missing files

---
+++ src/json/walker/log/trace.ts
@@ -0,0 +1,21 @@
+import { Context } from "../context";
+
+export function trace(
+  ctx: Context | null,
+  context: string,
+  message: string,
+): void {
+  const count = ctx ? ctx.getStack().length : 0;
+  const logMessage = " ".repeat(count) + `(${count})` + context + " " + message;
+  console.log(logMessage);
+}
+
+export function warn(
+  ctx: Context | null,
+  context: string,
+  message: string,
+): void {
+  const count = ctx ? ctx.getStack().length : 0;
+  const logMessage = " ".repeat(count) + context + " " + message;
+  console.warn(logMessage);
+}
--- src/parser/nodes/props/types.d.ts
+++
@@ -1,3 +0,0 @@
-export type ReferenceObject = {
-  $ref: string;
-};
